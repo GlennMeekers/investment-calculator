3d00bb9cf94d92d3bd902fe139ab0d9b531f4ec5
GlennMeekers <glennmeekers@gmail.com>
2025-03-28 11:28:14 +0100

    added input fields, lifted input states up to app and fed into calculateInvestmentResults function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import InputTable from "./components/InputTable";
 import OutputTable from "./components/OutputTable";
+import { calculateInvestmentResults } from "./util/investment.js";
 
 function App() {
+  const [formData, setFormData] = useState({
+    initialInvestment: null,
+    annualInvestment: null,
+    expectedReturn: null,
+    duration: null,
+  });
+
+  let outputData = calculateInvestmentResults(formData);
+
+  function inputHandler(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [name]: value,
+      };
+    });
+  }
+
   return (
     <>
       <Header />
+      <InputTable onInputChange={inputHandler} />
     </>
   );
 }
